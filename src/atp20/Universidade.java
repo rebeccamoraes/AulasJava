bf113a989e1228f3c89ba933f33338566db0bb10
Rebecca Moraes <rebeccagabrielle@gmail.com>
2021-10-05 16:37:48 -0300

    ATP20 - Criando classe Universidade e objetos do tipo Funcionario, Coordenador e Aluno.

---
+++ src/atp20/Universidade.java
@@ -0,0 +1,16 @@
+package atp20;
+
+/**
+ * Atividade Prática 20 - Herança
+ * 1. Crie uma classe Universidade, adicione um método main
+ * 2. Crie um objeto de Funcionario, um objeto de Coordenador e um objeto de Aluno.
+ * 3. Utilize os métodos set dos objetos de Funcionario, Coordenador e Aluno para inserir valores.
+ * 4.Imprima todos os dados dos objetos, utilizando seu metodos get, ao final da execução.
+ */
+public class Universidade {
+    public static void main(String[] args) {
+        Funcionario funcionario = new Funcionario();
+        Coordenador coordenador = new Coordenador();
+        Aluno aluno = new Aluno();
+    }
+}
