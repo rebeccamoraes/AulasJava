48939e12b9b93ff9862ca301cd1509975cd00289
Rebecca Moraes <rebeccagabrielle@gmail.com>
2021-10-05 09:28:23 -0300

    ATP19 - Gravando inteiros em um array.

---
+++ src/atp19/Numeros.java
@@ -0,0 +1,30 @@
+package atp19;
+
+import java.util.Scanner;
+
+/**
+ * Atividade Prática 19 - Arrays
+ * 
+ * 1. Crie uma classe Números com um método main.
+ * 2. Crie nesta classe um array de números inteiros com 10 posições.
+ * 3. Adicione um valor para cada posição do array.
+ * 4. Imprima ao final da execução do main, todas as posições do array utilizando o laço de repetição for.
+ */
+public class Numeros {
+    public static void main(String[] args) {
+        int[] numeros = new int[10];
+        Scanner scanner = new Scanner(System.in);
+
+        for (int i = 0; i < numeros.length; i++) {
+            System.out.printf("Informe o %do número: ", i+1);
+            numeros[i] = Integer.parseInt(scanner.nextLine());
+        }
+
+        System.out.println("Números digitados:");
+        for (int numero : numeros) {
+            System.out.printf("%d ", numero);            
+        }
+
+        scanner.close();
+    }
+}
