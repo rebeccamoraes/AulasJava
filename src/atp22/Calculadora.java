40ec730cc6f1edf9ccc3982d99ce277da184b12b
Rebecca Moraes <rebeccagabrielle@gmail.com>
2021-10-07 17:19:52 -0300

    ATP22 - Criando classe calculadora e metodo soma de inteiros.

---
+++ src/atp22/Calculadora.java
@@ -0,0 +1,17 @@
+package atp22;
+
+/**
+ * Atividade Prática 22 - Sobrecarga, Sobrescrita e Construtores
+ * 
+ * 1. Crie uma classe Calculadora.
+ * 2. Crie o método soma que recebe dois parâmetros inteiros e retorna um inteiro com o resultado.
+ * 3. Crie o método soma que recebe dois parâmetros double e retorna um double com o resultado.
+ * 4. Crie o método soma que recebe três parâmetros float e retorna um float com o resultado.
+ * 5. Crie o método soma que recebe três parâmetros long e retorna um long com o resultado.
+ */
+public class Calculadora {
+    public int soma(int valor1, int valor2) {
+        int resultado = valor1 + valor2
+        return resultado;
+    }
+}
