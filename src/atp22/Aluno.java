66177aefc86002a9dfd9bdbced75b8173b2d7b67
Rebecca Moraes <rebeccagabrielle@gmail.com>
2021-10-07 15:59:07 -0300

    ATP22 - Criação dos getters e setters na classe aluno.

--- src/atp22/Aluno.java
+++ src/atp22/Aluno.java
@@ -4,10 +4,32 @@ package atp22;
  * Atividade Prática 22 - Sobrecarga, Sobrescrita e Construtores
  * 
  * 1. Crie uma classe Aluno com os atributos privados nome completo, idade e matricula.
+ * 2. Crie os métodos getters e setters para cada atributo da classe Aluno.
  */
 
 public class Aluno {
     private String nomeCompleto;
     private int idade;
-    private String matricula;    
+    private String matricula;
+
+    public String getNomeCompleto() {
+        return nomeCompleto;
+    }
+    public void setNomeCompleto(String nomeCompleto) {
+        this.nomeCompleto = nomeCompleto;
+    }
+    
+    public int getIdade() {
+        return idade;
+    }
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+    
+    public String getMatricula() {
+        return matricula;
+    }
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
 }
