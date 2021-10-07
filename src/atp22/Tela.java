5a90da86b595d6e161d5db61f54dc587c9e0eab5
Rebecca Moraes <rebeccagabrielle@gmail.com>
2021-10-07 16:14:17 -0300

    ATP22 - Criando classe Tela e instanciando aluno.

---
+++ src/atp22/Tela.java
@@ -0,0 +1,13 @@
+package atp22;
+
+/**
+ * Atividade Prática 22 - Sobrecarga, Sobrescrita e Construtores
+ * 
+ * 1. Crie uma classe Tela com um método main.
+ * 2. Crie na classe Tela, 2 objetos da classe Aluno.
+ */
+public class Tela {
+    public static void main(String[] args) {
+        Aluno aluno = new Aluno();
+    }
+}
