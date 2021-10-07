7522fb2645cbc2253fe67e4c7384e1620c0fe82f
Rebecca Moraes <rebeccagabrielle@gmail.com>
2021-10-07 15:58:00 -0300

    ATP22 - Criação da classe aluno.

---
+++ src/atp22/Aluno.java
@@ -0,0 +1,13 @@
+package atp22;
+
+/**
+ * Atividade Prática 22 - Sobrecarga, Sobrescrita e Construtores
+ * 
+ * 1. Crie uma classe Aluno com os atributos privados nome completo, idade e matricula.
+ */
+
+public class Aluno {
+    private String nomeCompleto;
+    private int idade;
+    private String matricula;    
+}
