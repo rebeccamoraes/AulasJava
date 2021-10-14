27224505752e52a481293cc1b77e3dda6659e2ee
Rebecca Moraes <rebeccagabrielle@gmail.com>
2021-10-14 16:32:42 -0300

    ATP23 - Criando model de Livro.

---
+++ src/atp23/model/Livro.java
@@ -0,0 +1,67 @@
+package atp23.model;
+
+import java.util.Random;
+
+public class Livro {
+    private int id;
+    private String titulo;
+    private String autores;
+    private String editora;
+
+    public Livro() {
+        this.id = geraId();
+    }
+
+    public Livro(String titulo, String autores, String editora) {
+        this.id = geraId();
+        this.titulo = titulo;
+        this.autores = autores;
+        this.editora = editora;
+    }
+
+    public int getId() {
+        return id;
+    }
+    public int geraId() {
+        Random generator = new Random();
+        return generator.nextInt(10000);
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutores() {
+        return autores;
+    }
+    public void setAutores(String autores) {
+        this.autores = autores;
+    }
+
+    public String getEditora() {
+        return editora;
+    }
+    public void setEditora(String editora) {
+        this.editora = editora;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        Livro outroLivro = (Livro) obj;
+
+        boolean ehIgual = this.id == outroLivro.id;
+
+        return ehIgual;
+    }
+
+    @Override
+    public String toString() {
+        String texto = "Título: " + this.titulo
+                     + "\nAutores: " + this.autores
+                     + "\nEditora: " + this.editora + "\n";
+        return texto;
+    }
+}
