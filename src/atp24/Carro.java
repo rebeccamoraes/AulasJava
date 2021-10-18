b74396805ee39038145a7d61a4612cf126400b92
Rebecca Moraes <rebeccagabrielle@gmail.com>
2021-10-18 14:48:28 -0300

    ATP24 - Sobrescrevendo tostring de carro.

--- src/atp24/Carro.java
+++ src/atp24/Carro.java
@@ -8,4 +8,11 @@ package atp24;
 public class Carro {
     public int numeroPortas;
     public int capacidadePortaMalas;
+
+    @Override
+    public String toString() {
+        String texto = "Número de portas: " + this.numeroPortas
+                     + "\nCapacidade do porta-malas: " + this.capacidadePortaMalas + "\n";
+        return texto;
+    }
 }
