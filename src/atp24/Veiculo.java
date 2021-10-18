69d86292a8af4d3ce81a44a48744231eb43db7d6
Rebecca Moraes <rebeccagabrielle@gmail.com>
2021-10-18 14:18:07 -0300

    ATP24 - Criando classe veiculo.

---
+++ src/atp24/Veiculo.java
@@ -0,0 +1,12 @@
+package atp24;
+
+/**
+ * Atividade Prática 24 - Herança e conversão de objetos
+ * 
+ * 1. Crie uma classe veiculo com atributos públicos: marca, modelo e placa.
+ */
+public class Veiculo {
+    public String marca;
+    public String modelo;
+    public String placa;
+}
