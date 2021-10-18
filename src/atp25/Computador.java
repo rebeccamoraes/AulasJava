a84e6f6e7e715a1a6c803e0a786e6aad7af89f75
Rebecca Moraes <rebeccagabrielle@gmail.com>
2021-10-18 16:48:56 -0300

    ATP25 - Criando a classe computador.

---
+++ src/atp25/Computador.java
@@ -0,0 +1,12 @@
+package atp25;
+
+/**
+ * Atividade Prática 25 - Herança, conversão e sobrescrita
+ * 1. Crie uma classe computador com atributos públicos: marca, memoria ram, processador e disco rigido.
+ */
+public class Computador {
+    public String marca;
+    public String memoriaRam;
+    public String processador;
+    public String discoRigido;
+}
