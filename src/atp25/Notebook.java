8ef43ab5a701e6be595c1465cd6b6b0c1ef625ba
Rebecca Moraes <rebeccagabrielle@gmail.com>
2021-10-18 16:54:25 -0300

    Criando a classe notebook.

---
+++ src/atp25/Notebook.java
@@ -0,0 +1,13 @@
+package atp25;
+
+/**
+ * Atividade Prática 25 - Herança, conversão e sobrescrita
+ * 
+ * 1. Crie uma classe notebook que herda de computador com os atributos públicos: 
+ * bateria, velocidade wifi e versão bluetooth.
+ */
+public class Notebook extends Computador {
+    public String bateria;
+    public String velocidadeWifi;
+    public String versaoBluetooth;
+}
