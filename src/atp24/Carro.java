c7b6e202b45de6293a1fb15dedc155e6035e7eb2
Rebecca Moraes <rebeccagabrielle@gmail.com>
2021-10-18 14:19:33 -0300

    ATP24 - Criando classe carro.

---
+++ src/atp24/Carro.java
@@ -0,0 +1,11 @@
+package atp24;
+
+/**
+ * Atividade Prática 24 - Herança e conversão de objetos
+ * 
+ * 1. Crie uma classe carro com os atributos públicos: número de portas e capacidade do porta-malas.
+ */
+public class Carro {
+    public int numeroPortas;
+    public int capacidadePortaMalas;
+}
