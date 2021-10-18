842daddf79ae96b5025cfffd43b94cfe1d69fc6f
Rebecca Moraes <rebeccagabrielle@gmail.com>
2021-10-18 17:00:42 -0300

    Criando a classe desktop.

---
+++ src/atp25/Desktop.java
@@ -0,0 +1,13 @@
+package atp25;
+
+/**
+ * Atividade Prática 25 - Herança, conversão e sobrescrita
+ * 
+ * 1. Crie uma classe desktop que herda de computador com os atributos públicos: 
+ * tamanho gabinete, monitor externo e fonte.
+ */
+public class Desktop extends Computador {
+    public double tamanhoGabinete;
+    public String monitorExterno;
+    public String fonte;
+}
