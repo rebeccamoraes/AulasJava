391db71daed0cb1fc3070d4be71e4bbec1c5ac88
Rebecca Moraes <rebeccagabrielle@gmail.com>
2021-10-19 15:01:52 -0300

    ATP26 - Criaçcao da classe aluno.

---
+++ src/atp26/Aluno.java
@@ -0,0 +1,12 @@
+package atp26;
+
+/**
+ * Atividade Prática 26 - Conversão, sobrescrita e arrays
+ * 
+ * 1. Crie uma classe Aluno que herda de pessoa e que possua os atributos públicos: matricula, turma e curso
+ */
+public class Aluno extends Pessoa {
+    public String matricula;
+    public String turma;
+    public String curso;
+}
