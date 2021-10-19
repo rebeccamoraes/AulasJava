6c63cda494b59bee9f19f3fd90b0bc9064277d1a
Rebecca Moraes <rebeccagabrielle@gmail.com>
2021-10-19 14:59:13 -0300

    ATP26 - Criaçcao da classe pessoa.

---
+++ src/atp26/Pessoa.java
@@ -0,0 +1,12 @@
+package atp26;
+
+/**
+ * Atividade Prática 26 - 
+ * 
+ * 1. Crie uma classe Pessoa com os atributos públicos: nome, sobrenome e idade
+ */
+public class Pessoa {
+    public String nome;
+    public String sobrenome;
+    public int idade;
+}
