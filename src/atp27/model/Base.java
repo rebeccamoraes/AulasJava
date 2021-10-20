92aa109e68e87ab05e1e8b5fd5f95563347121fc
Rebecca Moraes <rebeccagabrielle@gmail.com>
2021-10-20 16:16:26 -0300

    ATP27 - Criando model base com id.

---
+++ src/atp27/model/Base.java
@@ -0,0 +1,16 @@
+package atp27.model;
+
+import java.util.Random;
+
+public abstract class Base {
+    private int id;
+
+    public Base() {
+        Random geradorID = new Random();
+        this.id = geradorID.nextInt(10000);
+    }
+
+    public int getId() {
+        return id;
+    }
+}
