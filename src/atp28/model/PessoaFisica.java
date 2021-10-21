5a89fb2b103f77a88e1fa13fde209d32515031ba
Rebecca Moraes <rebeccagabrielle@gmail.com>
2021-10-21 15:10:24 -0300

    ATP28 - Corrigindo construtor e adicionando documentacao.

--- src/atp28/model/PessoaFisica.java
+++ src/atp28/model/PessoaFisica.java
@@ -4,16 +4,29 @@ public class PessoaFisica extends Pessoa {
     public String nome;
     public String cpf;
 
+    /**
+     * Construtor gerando apenas id
+     */
     public PessoaFisica() {
         super();
     }
 
+    /**
+     * Construtor gerando id e salvando demais atributos
+     * @param nome
+     * @param cpf
+     * @param endereco
+     */
     public PessoaFisica(String nome, String cpf, Endereco endereco) {
+        super();
         this.nome = nome;
         this.cpf = cpf;
         this.endereco = endereco;
     }
 
+    /**
+     * Retorna os dados de pessoa
+     */
     @Override
     public String toString() {
         String texto = "PessoaFisica \nNome: " + nome + "\nCPF: " + cpf + "\n" + endereco + "\n";
