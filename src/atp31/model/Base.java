23d9fe524823c6492a66316a33dd4885eb46dd9f
Rebecca Moraes <rebeccagabrielle@gmail.com>
2021-10-26 08:54:10 -0300

    ATP31 - Criando classe base com gerador de id.

---
+++ src/atp31/model/Base.java
@@ -0,0 +1,28 @@
+package atp31.model;
+
+import java.util.Random;
+
+/**
+ * Estrutura basica de classes com id
+ */
+public abstract class Base {
+    private int id;
+
+    public Base() {
+        this.id = geraID();
+    }
+
+    private int geraID() {
+        Random gerador = new Random();
+        return gerador.nextInt(1000);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof Base) {
+            Base outro = (Base) obj;
+            return outro.id == this.id;
+        }
+        return false;
+    }
+}
