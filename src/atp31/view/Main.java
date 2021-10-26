5c325cefd9354aa1d89f4df6912859bc32bbc17e
Rebecca Moraes <rebeccagabrielle@gmail.com>
2021-10-26 11:14:43 -0300

    ATP31 - Criando estrutura do menu.

---
+++ src/atp31/view/Main.java
@@ -0,0 +1,84 @@
+package atp31.view;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        do {
+            int opcao = menu(scanner);
+
+            switch(opcao) {
+                case 1:
+                    System.out.println("---- Cadastro de produto ----");
+                    break;
+                
+                case 2:
+                    System.out.println("---- Atualização de produto ----");
+                    break;
+
+                case 3:
+                    System.out.println("---- Exclusão de produto ----");
+                    break;
+                
+                case 4:
+                    System.out.println("---- Lista de produtos ----");
+                    break;
+            }
+        } while(voltarAoMenu(scanner));
+    }
+
+    /**
+     * Exibe o menu e lê uma opção
+     * 
+     * @param scanner entrada de dados
+     * @return opção escolhida
+     */
+    private static int menu(Scanner scanner) {
+        int opcao = 0;
+        boolean opcaoInvalida = false;
+
+        do {
+            String mensagem = "----- Menu Produtos -----\n"
+                            + "\t1. Criar \n\t2. Atualizar \n\t3. Excluir \n\t4. Listar"
+                            + "\nEscolha uma opção: ";
+            
+            opcao = lerInteiro(scanner, mensagem);
+
+            opcaoInvalida = opcao < 1 || opcao > 4;
+
+            if (opcaoInvalida) {
+                System.out.println("Opção inválida! Tente novamente.");
+            }
+        } while (opcaoInvalida);
+
+        return opcao;
+    }
+    
+    private static boolean voltarAoMenu(Scanner scanner) {
+        char resposta = ' ';
+
+        System.out.println("\nDeseja voltar ao menu? (S\\N)");
+        resposta = scanner.nextLine().toUpperCase().charAt(0);
+        System.out.println();
+
+        return resposta == 'S';
+    }
+
+    private static int lerInteiro(Scanner scanner, String mensagem) {
+        boolean numeroInvalido = false;
+        int numero = 0;
+        do {
+            try {
+                System.out.print(mensagem);
+                numero = Integer.parseInt(scanner.nextLine());
+                numeroInvalido = false; 
+            } catch (NumberFormatException e) {
+                System.out.println("Erro. O número precisa ser inteiro.");
+                numeroInvalido = true;
+            }
+        } while (numeroInvalido);
+        return numero;
+    }
+}
