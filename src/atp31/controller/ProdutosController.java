6033d035a48dc3d43eb1d820d5432ed6df7c3e18
Rebecca Moraes <rebeccagabrielle@gmail.com>
2021-10-26 11:05:53 -0300

    ATP31 - Criando controller de produtos.

---
+++ src/atp31/controller/ProdutosController.java
@@ -0,0 +1,10 @@
+package atp31.controller;
+
+import atp31.model.Produto;
+
+/**
+ * Controller de produtos, extendido de BaseControler
+ */
+public class ProdutosController extends BaseController<Produto> {
+    
+}
