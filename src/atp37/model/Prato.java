1df012b44833c715b7747505ec6481e1d59efa62
Rebecca Moraes <rebeccagabrielle@gmail.com>
2021-11-03 10:50:36 -0300

    ATP37 - Criando model de Prato.

---
+++ src/atp37/model/Prato.java
@@ -0,0 +1,61 @@
+package atp37.model;
+
+public class Prato extends Base {
+    private String nome;
+    private double valor;
+
+    /**
+     * Gera id
+     */
+    public Prato() {
+        super();
+    }
+
+    /**
+     * Gera id e inicializa atributos nome e valor
+     * 
+     * @param nome descreve o prato
+     * @param valor preço do prato
+     */
+    public Prato(String nome, double valor) {
+        super();
+        this.nome = nome;
+        this.valor = valor;
+    }
+
+    /**
+     * Obtém o nome do prato
+     * 
+     * @return String nome
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * Atualiza o nome do prato
+     * 
+     * @param nome String novo nome
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    /**
+     * Obtém o valor do prato
+     * 
+     * @return double valor
+     */
+    public double getValor() {
+        return valor;
+    }
+
+    /**
+     * Atualiza o valor do prato
+     * 
+     * @param valor double novo valor
+     */
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+}
