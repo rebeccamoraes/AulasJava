cbad950bb44ac2ae57a581998dd32a659c089a80
Rebecca Moraes <rebeccagabrielle@gmail.com>
2021-11-03 13:58:26 -0300

    Aula 22

---
+++ src/modulo2/aula22/Eletrodomestico.java
@@ -0,0 +1,13 @@
+package modulo2.aula22;
+
+public class Eletrodomestico extends Produto {
+    private String voltagem;
+
+    public String getVoltagem() {
+        return voltagem;
+    }
+
+    public void setVoltagem(String voltagem) {
+        this.voltagem = voltagem;
+    }
+}
---
+++ src/modulo2/aula22/Produto.java
@@ -0,0 +1,11 @@
+package modulo2.aula22;
+
+public class Produto {
+    private String nome;
+    private String descricao;
+
+    public Produto(String nome, String descricao) {
+        this.nome = nome;
+        this.descricao = descricao;
+    }
+}
