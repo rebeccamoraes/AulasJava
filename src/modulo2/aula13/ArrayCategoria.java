d6cbc0db635e0d299ebfc7dce6b75980ab43a681
Rebecca Moraes <rebeccagabrielle@gmail.com>
2021-11-03 13:58:10 -0300

    Aula 13

---
+++ src/modulo2/aula13/ArrayCategoria.java
@@ -0,0 +1,20 @@
+package modulo2.aula13;
+
+public class ArrayCategoria {
+    public static void main(String[] args) {
+        Categoria[] categorias = new Categoria[5];
+
+        Categoria cat1 = new Categoria("Eletro", "Eletroeletronico");
+        Categoria cat2 = new Categoria("Smartphones", "Telefones que tem apps");
+
+        categorias[0] = cat1;
+        categorias[1] = cat2;
+        categorias[2] = new Categoria();
+        categorias[3] = new Categoria("Calçados", "Sapatos e tenis");
+        categorias[4] = new Categoria();
+
+        for (Categoria categoria: categorias) {
+            System.out.printf("Nome: %s \nDescrição: %s\n", categoria.getNome(), categoria.getDescricao());
+        }
+    }
+}
---
+++ src/modulo2/aula13/ArrayCategoriaScanner.java
@@ -0,0 +1,11 @@
+package modulo2.aula13;
+
+public class ArrayCategoriaScanner {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        Categoria[] categorias = new Categoria[3];
+
+        
+    } 
+}
