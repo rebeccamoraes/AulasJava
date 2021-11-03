6f01eb1a3f752abf421d8ebb2e525f2816388e05
Rebecca Moraes <rebeccagabrielle@gmail.com>
2021-11-03 16:37:48 -0300

    Aula28 - Escrita de arquivo com PrintWriter.

---
+++ src/modulo2/aula28/view/EscritaPrintWriter.java
@@ -0,0 +1,20 @@
+package modulo2.aula28.view;
+
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+
+public class EscritaPrintWriter {
+    public static void main(String[] args) {
+        PrintWriter ps = null;
+        try {
+            ps = new PrintWriter("./src/modulo2/aula28/dados/molhos.txt");
+            ps.println("Sugo");
+            ps.println("Branco");
+            ps.println("Bolonhesa");
+        } catch (FileNotFoundException e) {
+            System.out.println("Arquivo não encontrado.");
+        } finally {
+            ps.close();
+        }
+    }
+}
