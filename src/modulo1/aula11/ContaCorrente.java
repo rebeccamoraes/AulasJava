cfbcad626268618fdb462439a8c845e1d359a7b7
Rebecca Moraes <rebeccagabrielle@gmail.com>
2021-11-03 13:57:47 -0300

    Aula 11

---
+++ src/modulo1/aula11/ContaCorrente.java
@@ -0,0 +1,13 @@
+package modulo1.aula11;
+
+public class ContaCorrente {
+    private double saldo;
+
+    public void deposita(double valor) {
+        this.saldo += valor;
+    }
+
+    public void saca(double valor) {
+        this.saldo -= valor;
+    }
+}
---
+++ src/modulo1/aula11/Principal.java
@@ -0,0 +1,7 @@
+package modulo1.aula11;
+
+public class Principal {
+    public static void main(String[] args) {
+        
+    }
+}
