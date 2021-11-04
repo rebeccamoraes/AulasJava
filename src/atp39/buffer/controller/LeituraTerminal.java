53a2d5cc26522acc0d8627632dbc30f8f2ed6e35
Rebecca Moraes <rebeccagabrielle@gmail.com>
2021-11-04 10:27:04 -0300

    ATP39 - Documentando classe LeituraTerminal.

--- src/atp39/buffer/controller/LeituraTerminal.java
+++ src/atp39/buffer/controller/LeituraTerminal.java
@@ -2,17 +2,30 @@ package atp39.buffer.controller;
 
 import java.util.Scanner;
 
+/**
+ * Leitura de dados via terminal
+ */
 public class LeituraTerminal {
     Scanner scanner;
     
+    /**
+     * Cria instancia de scanner para ler dados via terminal
+     */
     public LeituraTerminal() {
         this.scanner = new Scanner(System.in);
     }
     
+    /**
+     * Lê dados do terminal e retorna em uma string
+     * 
+     * @return String com dados lidos via terminal
+     */
     public String ler() {
         String texto = "";
+
         while(scanner.hasNextLine()) {
             String nextLine = scanner.nextLine();
+
             if(nextLine.equals("q")) {
                 return texto;
             } else {
