a0e6e4994f558e6eb3b976d2b2ed67ba6918a9b0
Rebecca Moraes <rebeccagabrielle@gmail.com>
2021-11-12 14:58:05 -0300

    Aula 45 - Refazendo Delete com fabrica de conexão.

---
+++ src/modulo7/aula47/view/ViewDelete.java
@@ -0,0 +1,28 @@
+package modulo7.aula47.view;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
+import modulo7.aula47.utils.ConnectionFactory;
+
+public class View1 {
+    public static void main(String[] args) {
+        try (Connection conn = new ConnectionFactory().getConnection()) {
+            int idDeletado = 27;
+
+            String sql = "DELETE FROM categoria WHERE id = ?";
+
+            try (PreparedStatement prepStatement = conn.prepareStatement(sql)) {
+                prepStatement.setInt(1, idDeletado);
+                prepStatement.execute();
+
+                int linhasAfetadas = prepStatement.getUpdateCount();
+                System.out.println(linhasAfetadas);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
