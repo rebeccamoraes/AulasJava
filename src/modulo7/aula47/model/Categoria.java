52fe4dd48edc0fbd106f782b1ef57ded63d0a1bc
Rebecca Moraes <rebeccagabrielle@gmail.com>
2021-11-12 17:31:25 -0300

    Aula 47 - Criando construtores em categoria.

--- src/modulo7/aula47/model/Categoria.java
+++ src/modulo7/aula47/model/Categoria.java
@@ -3,6 +3,15 @@ package modulo7.aula47.model;
 public class Categoria {
     private int id;
     private String nome;
+
+    public Categoria(int id) {
+        this.id = id;
+    }    
+
+    public Categoria(int id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
     
     public void setId(int id) {
         this.id = id;
