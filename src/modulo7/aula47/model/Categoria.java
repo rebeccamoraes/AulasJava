ef05eb0a61345bedb375ca480603762257476232
Rebecca Moraes <rebeccagabrielle@gmail.com>
2021-11-12 17:29:32 -0300

    Aula 47 - Criando model de categoria.

---
+++ src/modulo7/aula47/model/Categoria.java
@@ -0,0 +1,20 @@
+package modulo7.aula47.model;
+
+public class Categoria {
+    private int id;
+    private String nome;
+    
+    public void setId(int id) {
+        this.id = id;
+    }
+    public int getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
