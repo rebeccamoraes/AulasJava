324e34ac9cebc58d0cd433c1162917f837a900c5
Rebecca Moraes <rebeccagabrielle@gmail.com>
2021-11-15 14:58:25 -0300

    Aulas

---
+++ src/modulo7/aula47/utils/CategoriaDao.java
@@ -0,0 +1,16 @@
+package modulo7.aula47.utils;
+
+import java.util.ArrayList;
+
+import modulo7.aula47.model.Categoria;
+
+public class CategoriaDao {
+    public void create() {
+
+    }
+
+    public ArrayList<Categoria> read() {
+        ArrayList<Categoria> lista = new ArrayList<Categoria>();
+        return lista;
+    }
+}
---
+++ src/modulo7/aula47/view/ViewInsert.java
@@ -0,0 +1,32 @@
+package modulo7.aula47.view;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import modulo7.aula47.model.Categoria;
+import modulo7.aula47.utils.ConnectionFactory;
+
+public class ViewInsert {
+    public static void main(String[] args) {
+        try (Connection conn = new ConnectionFactory().getConnection()) {
+            Categoria categoria = new Categoria("Calçados");
+
+            String sql = "INSERT INTO categoria (nome) VALUES (?)";
+            PreparedStatement prepStatement = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            prepStatement.setString(1, categoria.getNome());
+
+            prepStatement.execute();
+
+            ResultSet ids = prepStatement.getGeneratedKeys();
+            
+            while(ids.next()) {
+                System.out.printf("ID: %d\n", ids.getInt(1));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
