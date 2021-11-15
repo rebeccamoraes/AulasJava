b5d70f004aa3f47d03c8eef81bbf05c3d565b5e7
Rebecca Moraes <rebeccagabrielle@gmail.com>
2021-11-15 16:45:28 -0300

    ATP49 - Corrigindo factory.

--- src/atividades/modulo8/atp49/utils/ConnectionFactory.java
+++ src/atividades/modulo8/atp49/utils/ConnectionFactory.java
@@ -1,28 +1,16 @@
 package atividades.modulo8.atp49.utils;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.SQLException;
-
-import javax.sql.DataSource;
-
-import com.mchange.v2.c3p0.ComboPooledDataSource;
 
 public class ConnectionFactory {
     private String url = "jdbc:postgresql://localhost:5432/postgres";
     private String user = "postgres";
     private String password = "123456";
-    private DataSource dataSource;
-
-    public ConnectionFactory() {
-        ComboPooledDataSource pool = new ComboPooledDataSource();
-        pool.setJdbcUrl(url);
-        pool.setUser(user);
-        pool.setPassword(password);
-        pool.setMaxPoolSize(10);
-        this.dataSource = pool;
-    }
 
     public Connection getConnection() throws SQLException{
-        return this.dataSource.getConnection();
+        Connection conn = DriverManager.getConnection(url, user, password);
+        return conn;
     }
 }
