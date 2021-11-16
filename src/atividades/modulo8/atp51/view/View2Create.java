8deb9c23b003e99a1161ad1c3e9a5ee34f41ca49
Rebecca Moraes <rebeccagabrielle@gmail.com>
2021-11-16 17:34:40 -0300

    ATP51 - Criando cópia dos arquivos da atp50.

---
+++ src/atividades/modulo8/atp51/view/View2Create.java
@@ -0,0 +1,33 @@
+package atividades.modulo8.atp51.view;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import atividades.modulo8.atp49.utils.ConnectionFactory;
+
+public class View2Create {
+    public static void main(String[] args) {
+        try (Connection conn = new ConnectionFactory().getConnection()) {
+            String sql = "INSERT INTO categoria (nome) VALUES (?)";
+
+            PreparedStatement pStatement = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            pStatement.setString(1, "Alimentos");
+
+            pStatement.execute();
+
+            ResultSet ids = pStatement.getGeneratedKeys();
+
+            while (ids.next()) {
+                int id = ids.getInt(1);
+                System.out.printf("Id: %d\n", id);
+            }
+
+            conn.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/atividades/modulo8/atp51/view/View3Update.java
@@ -0,0 +1,28 @@
+package atividades.modulo8.atp51.view;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import atividades.modulo8.atp49.utils.ConnectionFactory;
+
+public class View3Update {
+    public static void main(String[] args) {
+        try (Connection conn = new ConnectionFactory().getConnection()) {
+            String sql = "UPDATE categoria SET nome= ? WHERE nome = ?";
+            PreparedStatement pStatement = conn.prepareStatement(sql);
+            String nomeAntigo = "Alimentos";
+            String nomeNovo = "Comidas";
+            pStatement.setString(1, nomeNovo);
+            pStatement.setString(2, nomeAntigo);
+            pStatement.execute();
+
+            int linhasAfetadas = pStatement.getUpdateCount();
+            System.out.println(linhasAfetadas);
+
+            conn.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
