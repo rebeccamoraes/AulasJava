c54bdf4d273ebf41ba3c4650f5184fc48f3f52ac
Rebecca Moraes <rebeccagabrielle@gmail.com>
2021-11-16 17:57:44 -0300

    ATP51 - Implementando create no dao de categoria.

--- src/atividades/modulo8/atp51/dao/CategoriaDAO.java
+++ src/atividades/modulo8/atp51/dao/CategoriaDAO.java
@@ -5,16 +5,46 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 import atividades.modulo8.atp51.utils.ConnectionFactory;
 
 import atividades.modulo8.atp51.model.Categoria;
 
 public class CategoriaDAO {
-    public void create() {
+    /**
+     * Cria uma nova categoria e salva no banco de dados
+     */
+    public int create(Categoria categoria) {
+        int idGerado = 0;
 
+        try (Connection conn = new ConnectionFactory().getConnection()) {
+            String sql = "INSERT INTO categoria (nome, descricao) VALUES (?, ?)";
+
+            PreparedStatement pStatement = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            pStatement.setString(1, categoria.getNome());
+            pStatement.setString(2, categoria.getDescricao());
+
+            pStatement.execute();
+
+            ResultSet ids = pStatement.getGeneratedKeys();
+
+            while (ids.next()) {
+                idGerado = ids.getInt(1);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return idGerado;
     }
 
+    /**
+     * Busca lista das categorias cadastradas no banco de dados
+     * 
+     * @return ArrayList: lista de categorias
+     */
     public ArrayList<Categoria> read() {
         ArrayList<Categoria> list = new ArrayList<Categoria>();
 
--- src/atividades/modulo8/atp51/view/View2Create.java
+++ src/atividades/modulo8/atp51/view/View2Create.java
@@ -1,33 +1,18 @@
 package atividades.modulo8.atp51.view;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-
-import atividades.modulo8.atp49.utils.ConnectionFactory;
+import atividades.modulo8.atp51.dao.CategoriaDAO;
+import atividades.modulo8.atp51.model.Categoria;
 
 public class View2Create {
     public static void main(String[] args) {
-        try (Connection conn = new ConnectionFactory().getConnection()) {
-            String sql = "INSERT INTO categoria (nome) VALUES (?)";
+        CategoriaDAO dao = new CategoriaDAO();
+        Categoria categoria = new Categoria();
+        
+        categoria.setNome("Smartphones");
+        categoria.setDescricao("Celulares inteligentes.");
 
-            PreparedStatement pStatement = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
-            pStatement.setString(1, "Alimentos");
+        categoria.setId(dao.create(categoria));
 
-            pStatement.execute();
-
-            ResultSet ids = pStatement.getGeneratedKeys();
-
-            while (ids.next()) {
-                int id = ids.getInt(1);
-                System.out.printf("Id: %d\n", id);
-            }
-
-            conn.close();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+        System.out.println(categoria);
     }
 }
