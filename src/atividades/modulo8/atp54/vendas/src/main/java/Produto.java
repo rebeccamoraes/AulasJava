d40e5b4f58ba44f52985b8617d8ff601a96cb130
Rebecca Moraes <rebeccagabrielle@gmail.com>
2021-11-22 16:06:13 -0300

    ATP54 - Criando servlets com parametros.

---
+++ src/atividades/modulo8/atp54/vendas/src/main/java/Produto.java
@@ -0,0 +1,30 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = "/produto")
+public class Produto extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String nome = req.getParameter("nome");
+        String valorString = req.getParameter("valor");
+        String idCategoriaString = req.getParameter("id_categoria");
+
+        PrintWriter out = resp.getWriter();
+        out.printf("Modulo Produto\nNome: %s\n", nome);
+
+        if(valorString != null) {
+            float valor = Float.parseFloat(valorString);
+            out.printf("Valor: %.2f\n", valor);
+        }
+        if(idCategoriaString != null) {
+            int idCategoria = Integer.parseInt(idCategoriaString);
+            out.printf("Id categoria: %d", idCategoria);
+        }
+    }
+}
---
+++ src/atividades/modulo8/atp54/vendas/src/main/webapp/index.jsp
@@ -0,0 +1,10 @@
+<html>
+    <head>
+        <title>Categorias e Produtos</title>
+    </head>
+<body>
+    <h2>Menu</h2>
+    <a href="/vendas-1/categoria?nome=Carros&descricao=automoveis">Categoria</a>
+    <a href="/vendas-1/produto?nome=Fusca&id_categoria=13&valor=13999">Produto</a>
+</body>
+</html>
