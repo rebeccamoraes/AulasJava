80d4bb51bf7041f5edf228bd9e142880187e5478
Rebecca Moraes <rebeccagabrielle@gmail.com>
2021-11-25 17:21:35 -0300

    ATP59 - Criando estrutura do projeto.

---
+++ src/atividades/modulo9/atp59/atp59/src/main/java/com/capgemini/rebecca/App.java
@@ -0,0 +1,13 @@
+package com.capgemini.rebecca;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
