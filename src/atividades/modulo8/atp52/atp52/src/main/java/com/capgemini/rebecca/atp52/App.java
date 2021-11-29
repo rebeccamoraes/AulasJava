bdd15f77503bb389816c9bead11ba1a920dced95
Rebecca Moraes <rebeccagabrielle@gmail.com>
2021-11-29 14:21:16 -0300

    ATP52 - Adicionando dao, model e view e ajustando para execução.

--- src/atividades/modulo8/atp52/atp52/src/main/java/com/capgemini/rebecca/atp52/App.java
+++ src/atividades/modulo8/atp52/atp52/src/main/java/com/capgemini/rebecca/atp52/App.java
@@ -1,13 +1,16 @@
 package com.capgemini.rebecca.atp52;
 
+import javax.swing.JFrame;
+
+import com.capgemini.rebecca.atp52.view.CategoriaView;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main( String[] args ) {
+        CategoriaView view = new CategoriaView();
+        view.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 }
