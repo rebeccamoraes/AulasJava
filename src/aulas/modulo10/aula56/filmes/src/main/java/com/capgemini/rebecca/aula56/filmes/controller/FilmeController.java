aee5085a67c040418feb712fbe8ebd7bedd34509
Rebecca Moraes <rebeccagabrielle@gmail.com>
2021-11-30 13:20:59 -0300

    Aula56 - Criando controller e página de filmes.

---
+++ src/aulas/modulo10/aula56/filmes/src/main/java/com/capgemini/rebecca/aula56/filmes/controller/FilmeController.java
@@ -0,0 +1,15 @@
+package com.capgemini.rebecca.aula56.filmes.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class FilmeController {
+    @GetMapping("/filme")
+    public String filmes(HttpServletRequest req) {
+        req.setAttribute("nome", "Esqueceram de mim");
+        return "filmes";
+    }
+}
