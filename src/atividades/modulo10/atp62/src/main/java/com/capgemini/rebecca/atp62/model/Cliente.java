782c052b29364f26a163ede4b4035f83cf9e0a2e
Rebecca Moraes <rebeccagabrielle@gmail.com>
2021-11-29 21:33:49 -0300

    ATP62 - Criando classe Cliente.

---
+++ src/atividades/modulo10/atp62/src/main/java/com/capgemini/rebecca/atp62/model/Cliente.java
@@ -0,0 +1,44 @@
+package com.capgemini.rebecca.atp62.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "cliente")
+public class Cliente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String nome;
+    private String cpf;
+    private int idade;
+    
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getCpf() {
+        return cpf;
+    }
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+    public int getIdade() {
+        return idade;
+    }
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+    
+}
