1a4008e3ea451ff4662a8e9ff6710921f93ffe7c
Rebecca Moraes <rebeccagabrielle@gmail.com>
2021-11-29 23:18:00 -0300

    ATP63 - Criando repository de agencia.

---
+++ src/atividades/modulo10/atp63/src/main/java/com/capgemini/rebecca/atp62/repository/AgenciaRepository.java
@@ -0,0 +1,9 @@
+package com.capgemini.rebecca.atp62.repository;
+
+import com.capgemini.rebecca.atp62.model.Agencia;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface AgenciaRepository extends CrudRepository<Agencia, Integer> {
+    
+}
