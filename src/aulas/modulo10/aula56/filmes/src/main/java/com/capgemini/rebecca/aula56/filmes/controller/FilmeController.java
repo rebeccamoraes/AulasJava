b03ce35254b4ef4df1219699ff26e90edb4e7cc3
Rebecca Moraes <rebeccagabrielle@gmail.com>
2021-11-30 13:31:06 -0300

    Aula56 - Listando filmes com thymeleaf.

--- src/aulas/modulo10/aula56/filmes/src/main/java/com/capgemini/rebecca/aula56/filmes/controller/FilmeController.java
+++ src/aulas/modulo10/aula56/filmes/src/main/java/com/capgemini/rebecca/aula56/filmes/controller/FilmeController.java
@@ -1,15 +1,27 @@
 package com.capgemini.rebecca.aula56.filmes.controller;
 
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
+
+import com.capgemini.rebecca.aula56.filmes.model.Filme;
+import com.capgemini.rebecca.aula56.filmes.repository.FilmeRepository;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class FilmeController {
+    FilmeRepository repository;
+
+    public FilmeController(FilmeRepository repository) {
+        this.repository = repository;
+    }
+
     @GetMapping("/filme")
     public String filmes(HttpServletRequest req) {
-        req.setAttribute("nome", "Esqueceram de mim");
+        List<Filme> lista = (List<Filme>)repository.findAll();
+        req.setAttribute("filmes", lista);
         return "filmes";
     }
 }
