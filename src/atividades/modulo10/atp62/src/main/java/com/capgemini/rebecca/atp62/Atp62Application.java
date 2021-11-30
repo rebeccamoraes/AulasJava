8ed8d176b4576efddfda554cc08fd4cb5b012ffb
Rebecca Moraes <rebeccagabrielle@gmail.com>
2021-11-29 21:42:58 -0300

    ATP62 - Testando cadastro e listagem de clientes.

--- src/atividades/modulo10/atp62/src/main/java/com/capgemini/rebecca/atp62/Atp62Application.java
+++ src/atividades/modulo10/atp62/src/main/java/com/capgemini/rebecca/atp62/Atp62Application.java
@@ -1,13 +1,34 @@
 package com.capgemini.rebecca.atp62;
 
+import com.capgemini.rebecca.atp62.model.Cliente;
+import com.capgemini.rebecca.atp62.repository.ClienteRepository;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Atp62Application {
+public class Atp62Application implements CommandLineRunner {
+	ClienteRepository repository;
 
+	public Atp62Application(ClienteRepository repository) {
+		this.repository = repository;
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(Atp62Application.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Cliente entity = new Cliente();
+		entity.setNome("Rebecca Moraes");
+		entity.setIdade(26);
+		entity.setCpf("123.456.789-10");
+
+		this.repository.save(entity);
+
+		this.repository.findAll().forEach(cliente->System.out.printf("Cliente %d: %s, %d anos, CPF %s\n", 
+		cliente.getId(), cliente.getNome(), cliente.getIdade(), cliente.getCpf()));
 	}
 
 }
