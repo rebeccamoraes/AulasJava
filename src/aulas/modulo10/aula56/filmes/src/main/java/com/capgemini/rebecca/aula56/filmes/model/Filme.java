3650e3d24094245870c59c640ee56dbfa221d9ea
Rebecca Moraes <rebeccagabrielle@gmail.com>
2021-11-30 13:21:28 -0300

    Aula56 - Criando model e repository de filmes.

---
+++ src/aulas/modulo10/aula56/filmes/src/main/java/com/capgemini/rebecca/aula56/filmes/model/Filme.java
@@ -0,0 +1,44 @@
+package com.capgemini.rebecca.aula56.filmes.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "filme")
+public class Filme {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String nome;
+    private String nomeDiretor;
+    private String genero;
+    
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getNomeDiretor() {
+        return nomeDiretor;
+    }
+    public void setNomeDiretor(String nomeDiretor) {
+        this.nomeDiretor = nomeDiretor;
+    }
+    public String getGenero() {
+        return genero;
+    }
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+}
