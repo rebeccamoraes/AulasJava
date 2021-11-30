1ab8e26af14ce7ca8b0a61cf258fcd04dcf9e637
Rebecca Moraes <rebeccagabrielle@gmail.com>
2021-11-29 23:52:02 -0300

    ATP64 - Criando estrutura do projeto com start.spring.io.

---
+++ src/atividades/modulo10/atp64/src/test/java/com/capgemini/rebecca/atp64/Atp64ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capgemini.rebecca.atp64;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Atp64ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
