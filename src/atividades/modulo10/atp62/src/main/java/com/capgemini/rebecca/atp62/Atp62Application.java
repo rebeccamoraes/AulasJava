bf9d05ded83d5bc9e7a0130dfd9eb6e969cf84ab
Rebecca Moraes <rebeccagabrielle@gmail.com>
2021-11-29 21:18:56 -0300

    ATP62 - Projeto criado com start.spring.io.

---
+++ src/atividades/modulo10/atp62/src/main/java/com/capgemini/rebecca/atp62/Atp62Application.java
@@ -0,0 +1,13 @@
+package com.capgemini.rebecca.atp62;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Atp62Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Atp62Application.class, args);
+	}
+
+}
---
+++ src/atividades/modulo10/atp62/src/test/java/com/capgemini/rebecca/atp62/Atp62ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capgemini.rebecca.atp62;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Atp62ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
