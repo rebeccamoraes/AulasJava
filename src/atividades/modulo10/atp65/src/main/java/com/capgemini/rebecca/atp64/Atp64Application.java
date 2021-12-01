6425565afcc4e5be6bb71ac6f3484509873d3462
Rebecca Moraes <rebeccagabrielle@gmail.com>
2021-11-30 21:10:10 -0300

    ATP65 - Criando projeto a partir da ATP64 e adicionando descrição da atividade.

---
+++ src/atividades/modulo10/atp65/src/main/java/com/capgemini/rebecca/atp64/Atp64Application.java
@@ -0,0 +1,13 @@
+package com.capgemini.rebecca.atp64;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Atp64Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Atp64Application.class, args);
+	}
+
+}
