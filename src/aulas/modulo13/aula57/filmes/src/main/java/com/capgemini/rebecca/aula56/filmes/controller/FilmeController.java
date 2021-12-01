30a03392696b44420e2585476a0c308b0263b51e
Rebecca Moraes <rebeccagabrielle@gmail.com>
2021-12-01 10:43:44 -0300

    Aula57 - Criando API Rest.

---
+++ src/aulas/modulo13/aula57/filmes/src/main/java/com/capgemini/rebecca/aula56/filmes/controller/FilmeController.java
@@ -0,0 +1,55 @@
+package com.capgemini.rebecca.aula56.filmes.controller;
+
+import java.util.List;
+
+import com.capgemini.rebecca.aula56.filmes.model.Filme;
+import com.capgemini.rebecca.aula56.filmes.repository.FilmeRepository;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class FilmeController {
+    FilmeRepository repository;
+
+    public FilmeController(FilmeRepository repository) {
+        this.repository = repository;
+    }
+
+    @GetMapping("/filme")
+    public String filmes(Model req) {
+        List<Filme> lista = (List<Filme>)repository.findAll();
+        req.addAttribute("filmes", lista);
+        return "filmes";
+    }
+
+    @GetMapping("/filme/form")
+    public String formulario(Model req) {
+        Filme model = new Filme();
+        req.addAttribute("filme", model);
+        return "filmes-form";
+    }
+
+    @PostMapping("/filme/salvar")
+    public String salvar(Filme model) {
+        System.out.println(model.getNome());
+        repository.save(model);
+        return "redirect:/filme";
+    }
+
+    @GetMapping("/filme/deletar/{id}")
+    public String deletar(@PathVariable int id) {
+        repository.deleteById(id);
+        return "redirect:/filme";
+    }
+
+    @GetMapping("/filme/editar/{id}")
+    public String editar(@PathVariable int id, Model req) {
+        Filme model = repository.findById(id).get();
+        req.addAttribute("filme", model);
+        return "/filmes-form";
+    }
+}
