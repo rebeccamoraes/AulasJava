0268311b452eea420967ceb24fd7a8d15a2848ef
Rebecca Moraes <rebeccagabrielle@gmail.com>
2021-12-01 20:35:27 -0300

    Aula58 - Adicionando ordenação na lista.

--- src/aulas/modulo13/aula58/filmes/src/main/java/com/capgemini/rebecca/aula58/filmes/repository/FilmeRepository.java
+++ src/aulas/modulo13/aula58/filmes/src/main/java/com/capgemini/rebecca/aula58/filmes/repository/FilmeRepository.java
@@ -2,8 +2,12 @@ package com.capgemini.rebecca.aula58.filmes.repository;
 
 import com.capgemini.rebecca.aula58.filmes.model.Filme;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
-public interface FilmeRepository extends CrudRepository<Filme, Integer> {
-    
+public interface FilmeRepository extends PagingAndSortingRepository<Filme, Integer> {
+    @Override
+    default Iterable<Filme> findAll() {
+        return findAll(Sort.by(Sort.Direction.ASC, "id"));
+    }
 }
