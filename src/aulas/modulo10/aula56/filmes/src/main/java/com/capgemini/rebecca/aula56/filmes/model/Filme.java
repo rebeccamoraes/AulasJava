aaf91f077f8bbab3e69cd83a307aa40976d847fb
Rebecca Moraes <rebeccagabrielle@gmail.com>
2021-11-30 21:00:44 -0300

    Aula56 - Substituindo getters e setters com lombok.

--- src/aulas/modulo10/aula56/filmes/src/main/java/com/capgemini/rebecca/aula56/filmes/model/Filme.java
+++ src/aulas/modulo10/aula56/filmes/src/main/java/com/capgemini/rebecca/aula56/filmes/model/Filme.java
@@ -6,39 +6,23 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import lombok.Getter;
+import lombok.Setter;
+
 @Entity
 @Table(name = "filme")
 public class Filme {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Getter @Setter
     private int id;
-    private String nome;
-    private String nomeDiretor;
-    private String genero;
-    
-    public int getId() {
-        return id;
-    }
-    public void setId(int id) {
-        this.id = id;
-    }
-    public String getNome() {
-        return nome;
-    }
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-    public String getNomeDiretor() {
-        return nomeDiretor;
-    }
-    public void setNomeDiretor(String nomeDiretor) {
-        this.nomeDiretor = nomeDiretor;
-    }
-    public String getGenero() {
-        return genero;
-    }
-    public void setGenero(String genero) {
-        this.genero = genero;
-    }
 
+    @Getter @Setter
+    private String nome;
+
+    @Getter @Setter
+    private String nomeDiretor;
+
+    @Getter @Setter
+    private String genero;
 }
