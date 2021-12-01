df7666b3cde7e0154061270c72501b4086df7bf3
Rebecca Moraes <rebeccagabrielle@gmail.com>
2021-12-01 20:27:56 -0300

    Aula58 - Adicionando Request Mapping na api.

--- src/aulas/modulo13/aula58/filmes/src/main/java/com/capgemini/rebecca/aula58/filmes/api/FilmeAPI.java
+++ src/aulas/modulo13/aula58/filmes/src/main/java/com/capgemini/rebecca/aula58/filmes/api/FilmeAPI.java
@@ -12,32 +12,34 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/filme")
 public class FilmeAPI {
     @Autowired //substitiu o construtor - injeção de dependência
     FilmeRepository repository;
 
-    @GetMapping("/api/filme")
+    @GetMapping
     public List<Filme> filmes() {
         List<Filme> lista = (List<Filme>)repository.findAll();
         return lista;
     }
 
-    @PostMapping("/api/filme")
+    @PostMapping
     public String salvar(@RequestBody Filme model) {
         repository.save(model);
         return "Salvo com sucesso.";
     }
 
-    @DeleteMapping("/api/filme/{id}")
+    @DeleteMapping("/{id}")
     public String delete(@PathVariable int id) {
         repository.deleteById(id);
         return "Deletado com sucesso.";
     }
 
-    @PutMapping("/api/filme/{id}")
+    @PutMapping("/{id}")
     public String update(@RequestBody Filme model, @PathVariable int id) {
         if(model.getId() == id) {
             repository.save(model);
