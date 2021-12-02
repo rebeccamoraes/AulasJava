838590cbda8672199eec3e9d8a7dedc2ef625f7a
Rebecca Moraes <rebeccagabrielle@gmail.com>
2021-12-01 22:00:22 -0300

    Aula56 - Criando models de genero e serie e relacionando um ao outro.

---
+++ src/aulas/modulo13/aula58/filmes/src/main/java/com/capgemini/rebecca/aula58/filmes/model/Serie.java
@@ -0,0 +1,30 @@
+package com.capgemini.rebecca.aula58.filmes.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "serie")
+public class Serie {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Getter @Setter
+    private int id;
+
+    @Getter @Setter
+    private String nome;
+
+    @Getter @Setter
+    private String nomeDiretor;
+
+    @Getter @Setter
+    @ManyToOne
+    private Genero genero;
+}
